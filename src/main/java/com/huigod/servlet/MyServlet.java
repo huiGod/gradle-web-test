2ee060f449e2ffc4981be83a63974d301b715ef3
huigod <tenghuiit@163.com>
2017-08-12 22:22:29 +0800

    gradle web project with gretty plugin

---
+++ src/main/java/com/huigod/servlet/MyServlet.java
@@ -0,0 +1,24 @@
+package com.huigod.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet invoked");
+        System.out.println("second line");
+        int a = 1;
+        int b = 2;
+
+        int c = a + b;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Test Page</title>
+  </head>
+  <body>
+  第一个测试页面,改完之后的效果
+  </body>
+</html>
